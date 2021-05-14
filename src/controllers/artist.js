2130cc5715cb47ecd5986da7c381621fe65ee6fa
Ernie <emetcalf44@gmail.com>
2021-05-14 10:38:02 +0100

    security added

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -7,7 +7,10 @@ exports.create = async (req, res) => {
     const { name, genre } = req.body;
 
     try {
-        await db.query(`INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
+        await db.query(`INSERT INTO Artist (name, genre) VALUES ('?, ?', [
+            name,
+            genre,
+        ])`);
         res.sendStatus(201);
     } catch (err) {
         res.sendStatus(500).json(err);
